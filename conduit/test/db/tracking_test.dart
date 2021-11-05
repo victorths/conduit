1a7c93758dbe1929e00b64484c2e3e0c59a77ea3
AnhQuan Nguyen <j4qfrost@gmail.com>
2021-11-05 01:15:26 -0700

    fix nulls

--- conduit/test/db/tracking_test.dart
+++ conduit/test/db/tracking_test.dart
@@ -18,8 +18,7 @@ void main() {
     test("Identify top-level", () {
       expect(
           context!
-              .entityForType(Parent)!
-              .identifyAttribute((Parent? x) => x!.field)
+              .entityForType(Parent).identifyAttribute((Parent? x) => x!.field)
               .name,
           "field");
     });
@@ -27,8 +26,7 @@ void main() {
     test("Cannot select relationship", () {
       try {
         context!
-            .entityForType(Child)!
-            .identifyAttribute((Child? p) => p!.parent);
+            .entityForType(Child).identifyAttribute((Child? p) => p!.parent);
         fail("unreachable");
       } on ArgumentError catch (e) {
         expect(e.toString(), contains("cannot be selected"));
@@ -38,8 +36,7 @@ void main() {
     test("Cannot nest attribute selection", () {
       try {
         context!
-            .entityForType(Child)!
-            .identifyAttribute((Child? p) => p!.parent!.field);
+            .entityForType(Child).identifyAttribute((Child? p) => p!.parent!.field);
         fail("unreachable");
       } on ArgumentError catch (e) {
         expect(e.toString(), contains("Cannot use relationships"));
@@ -48,7 +45,7 @@ void main() {
 
     test("cannot select multiple attributes", () {
       try {
-        context!.entityForType(Child)!.identifyAttribute((Child? p) {
+        context!.entityForType(Child).identifyAttribute((Child? p) {
           // ignore: unnecessary_statements
           p!.document;
           return p.field;
@@ -65,8 +62,7 @@ void main() {
     test("Can select document directly", () {
       expect(
           context!
-              .entityForType(Parent)!
-              .identifyAttribute((Parent? x) => x!.document)
+              .entityForType(Parent).identifyAttribute((Parent? x) => x!.document)
               .name,
           "document");
     });
@@ -74,8 +70,7 @@ void main() {
     test("Cannot select sub-document", () {
       try {
         context!
-            .entityForType(Child)!
-            .identifyAttribute((Child? p) => p!.document!["foo"]);
+            .entityForType(Child).identifyAttribute((Child? p) => p!.document!["foo"]);
         fail("unreachable");
       } on ArgumentError catch (e) {
         expect(e.toString(),
@@ -88,8 +83,7 @@ void main() {
     test("Identify top-level relationship", () {
       expect(
           context!
-              .entityForType(Parent)!
-              .identifyRelationship((Parent? x) => x!.children)
+              .entityForType(Parent).identifyRelationship((Parent? x) => x!.children)
               .name,
           "children");
     });
@@ -97,8 +91,7 @@ void main() {
     test("Identify top-level relationship to-one", () {
       expect(
           context!
-              .entityForType(Child)!
-              .identifyRelationship((Child? x) => x!.parent)
+              .entityForType(Child).identifyRelationship((Child? x) => x!.parent)
               .name,
           "parent");
     });
@@ -106,8 +99,7 @@ void main() {
     test("Cannot select attribute", () {
       try {
         context!
-            .entityForType(Parent)!
-            .identifyRelationship((Parent? p) => p!.document);
+            .entityForType(Parent).identifyRelationship((Parent? p) => p!.document);
         fail("unreachable");
       } on ArgumentError catch (e) {
         expect(e.toString(), contains("Invalid property selection"));
@@ -117,8 +109,7 @@ void main() {
     test("Cannot nest attribute selection", () {
       try {
         context!
-            .entityForType(Grandchild)!
-            .identifyRelationship((Grandchild? p) => p!.parent.parent);
+            .entityForType(Grandchild).identifyRelationship((Grandchild? p) => p!.parent.parent);
         fail("unreachable");
       } on ArgumentError catch (e) {
         expect(e.toString(), contains("Cannot identify a nested relationship"));
@@ -127,7 +118,7 @@ void main() {
 
     test("cannot select multiple attributes", () {
       try {
-        context!.entityForType(Child)!.identifyRelationship((Child? p) {
+        context!.entityForType(Child).identifyRelationship((Child? p) {
           // ignore: unnecessary_statements
           p!.parent;
           return p.grandchild;
@@ -144,8 +135,8 @@ void main() {
 
   group("KeyPath identification", () {
     test("Identify multiple properties", () {
-      final props = context!.entityForType(Parent)!.identifyProperties(
-          (Parent? x) => [x!.document, x.field, x.children])!;
+      final props = context!.entityForType(Parent).identifyProperties(
+          (Parent? x) => [x!.document, x.field, x.children]);
       expect(props.length, 3);
       expect(props.any((k) => k.path.first!.name == "document"), true);
       expect(props.any((k) => k.path.first!.name == "field"), true);
@@ -154,8 +145,7 @@ void main() {
 
     test("Identify top-level property with subdoc", () {
       final props = context!
-          .entityForType(Parent)!
-          .identifyProperties((Parent? x) => [x!.document!["k"]]);
+          .entityForType(Parent).identifyProperties((Parent? x) => [x!.document!["k"]]);
       expect(props.length, 1);
       expect(props.first.path.length, 1);
       expect(props.first.path.first!.name, "document");
@@ -164,8 +154,7 @@ void main() {
 
     test("Identify top-level property with subdoc", () {
       final props = context!
-          .entityForType(Parent)!
-          .identifyProperties((Parent? x) => [x!.document!["k"][1]]);
+          .entityForType(Parent).identifyProperties((Parent? x) => [x!.document!["k"][1]]);
       expect(props.length, 1);
       expect(props.first.path.length, 1);
       expect(props.first.path.first!.name, "document");
@@ -174,8 +163,7 @@ void main() {
 
     test("Subdoc + normal property", () {
       final props = context!
-          .entityForType(Parent)!
-          .identifyProperties((Parent? x) => [x!.document!["k"][1], x.field]);
+          .entityForType(Parent).identifyProperties((Parent? x) => [x!.document!["k"][1], x.field]);
       expect(props.length, 2);
 
       expect(props.first.path.length, 1);
@@ -189,8 +177,7 @@ void main() {
 
     test("Can select nested properties", () {
       final props = context!
-          .entityForType(Child)!
-          .identifyProperties((Child? x) => [x!.parent!.field]);
+          .entityForType(Child).identifyProperties((Child? x) => [x!.parent!.field]);
       expect(props.length, 1);
       expect(props.first.path.length, 2);
       expect(props.first.path.first!.name, "parent");
